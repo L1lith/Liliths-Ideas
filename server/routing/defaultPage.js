183934fe3e2a8d79694e228f40df04afc74a1814
L1lith <lily@lillith.pw>
2018-01-26 14:55:24 -0700

    move public folder into client

--- server/routing/defaultPage.js
+++ server/routing/defaultPage.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = function(app){
   app.get('*', function(req, res, next) {
     if (req.accepts('html')) {
-      res.status(200).sendFile('index.html',{root:path.join(__dirname,'..','/static/public/')});
+      res.status(200).sendFile('index.html',{root:path.join(__dirname,'../../client/public/')});
     } else {
       next();
     }
--- server/routing/public/static.js
+++ server/routing/public/static.js
@@ -2,6 +2,6 @@ const express = require('express');
 const path = require('path');
 
 module.exports = function(app){
-  app.use(express.static(path.join(__dirname,'../../static/public')));
+  app.use(express.static(path.join(__dirname,'../../../client/public')));
   app.use(express.static(path.join(__dirname,'../../static/build')));
 }
