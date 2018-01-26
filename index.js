2c313c90a8ac7465c1ea674b6ce30c3db1cf40f6
L1lith <lily@lillith.pw>
2018-01-25 22:46:23 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const minimist = require('minimist');
+
+const developmentConfig = require('./webpack.dev.js');
+const productionConfig = require('./webpack.prod.js');
+
+const args = minimist(process.argv.slice(2));
+const isDevelopment = [true,'true'].includes(args.dev);
+
+process.env.NODE_ENV = isDevelopment ? 'development' : 'production';
+
+const compiler = webpack(isDevelopment ? developmentConfig : productionConfig);
+
+console.log('Starting in '+(isDevelopment ? 'Development' : 'Production')+' mode.');
+startWebpack();
+
+function startWebpack() {
+  console.log('Starting Webpack');
+  if (isDevelopment) {
+    compiler.watch({},built);
+    startServer();
+  } else {
+    compiler.run(built);
+  }
+}
+
+function built(err,stats){
+  if (err || stats.hasErrors()) throw (err || new Error("Couldn't Start, Webpack Error"));
+  console.log('Webpack Built');
+  if (!isDevelopment) startServer();
+}
+
+var serverStarted = false;
+function startServer(){
+  if (serverStarted) throw new Error('Server Already Started');
+  serverStarted = true;
+  console.log('Starting Server');
+  require('./server').then(()=>{
+    console.log('Server Running');
+  }).catch(err=>{
+    throw (err || new Error("Couldn't Start, Server Error"));
+  });
+}
