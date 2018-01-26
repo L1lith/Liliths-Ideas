223f074107b807c138996a3b4bdbf5cbd7014df3
L1lith <lily@lillith.pw>
2018-01-26 15:35:03 -0700

    fix null handling in sanitize

--- server/functions/sanitize.js
+++ server/functions/sanitize.js
@@ -1,6 +1,7 @@
 function sanitize(object,format){
   if (typeof format == 'object') {
-    if ((format === null) != (object === null) || typeof format != typeof object || Array.isArray(format) != Array.isArray(object)) return false;
+    if (format === null) return object === null;
+    if (typeof format != typeof object || Array.isArray(format) != Array.isArray(object)) return false;
     return Object.entries(object).every(pair=>format.hasOwnProperty(pair[0])&&sanitize(pair[1],format[pair[0]]));
   } else if (typeof format == 'function') {
     return format(object) === true;
