7e6c647cb3fce4708a0d046c529a0241257373cf
L1lith <lily@lillith.pw>
2018-01-27 15:58:53 -0700

    cleanup validate route to use getUser

--- server/routing/auth/validate.js
+++ server/routing/auth/validate.js
@@ -1,17 +1,24 @@
 const secureRequest = require('../middleware/secureRequest');
+const getUser = require('../../functions/getUser');
 
 function validate(router, models) {
-  const {User} = models;
+  const {
+    User
+  } = models;
   router.get('/validate', secureRequest(models));
   router.get('/validate', (req, res) => {
-    User.findOne({username:res.locals.session.owner},(err,user)=>{
-      if (err || !user) return res.status(500).send('Internal Error');
-      const output = {
-        username: user.username,
-        displayname: user.displayname,
-        admin: user.admin
-      };
-      res.status(200).json(output);
+    getUser(res.locals.session, models).then(user => {
+      if (user) {
+        res.status(200).json({
+          username: user.username,
+          displayname: user.displayname,
+          admin: user.admin
+        });
+      } else {
+        res.status(401).send('Unauthorized');
+      }
+    }).catch(err => {
+      res.status(500).send('Internal Error');
     });
   });
 }
