8b80a52f74d29d5f6daf46bc89b50f60d14680fa
L1lith <lily@lillith.pw>
2018-01-26 21:13:36 -0700

    update auth security middleware to be method specific

--- server/routing/auth/logout.js
+++ server/routing/auth/logout.js
@@ -1,6 +1,6 @@
 const secureRequest = require('../middleware/secureRequest');
 function logout(router,models){
-  router.use('/logout',secureRequest(models));
+  router.get('/logout',secureRequest(models));
   router.get('/logout',(req,res)=>{
     res.locals.session.remove(err=>{
       if (err) return res.status(500).send('Internal Error');
--- server/routing/auth/validate.js
+++ server/routing/auth/validate.js
@@ -1,6 +1,6 @@
 const secureRequest = require('../middleware/secureRequest');
 function validate(router,models){
-  router.use('/validate',secureRequest(models));
+  router.get('/validate',secureRequest(models));
   router.get('/validate',(req,res)=>{
     res.status(200).send('Authorized');
   });
