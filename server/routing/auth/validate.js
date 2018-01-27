0a578a1fc501c1e8dc3be9a6b73c683153b7fc33
L1lith <lily@lillith.pw>
2018-01-27 16:06:22 -0700

    cleanup getUser

--- server/routing/auth/validate.js
+++ server/routing/auth/validate.js
@@ -1,24 +1,16 @@
 const secureRequest = require('../middleware/secureRequest');
-const getUser = require('../../functions/getUser');
 
 function validate(router, models) {
   const {
     User
   } = models;
-  router.get('/validate', secureRequest(models));
+  router.get('/validate', secureRequest(models,true));
   router.get('/validate', (req, res) => {
-    getUser(res.locals.session, models).then(user => {
-      if (user) {
-        res.status(200).json({
-          username: user.username,
-          displayname: user.displayname,
-          admin: user.admin
-        });
-      } else {
-        res.status(401).send('Unauthorized');
-      }
-    }).catch(err => {
-      res.status(500).send('Internal Error');
+    const user = res.locals.user;
+    res.status(200).json({
+      username: user.username,
+      displayname: user.displayname,
+      admin: user.admin
     });
   });
 }
--- server/routing/middleware/secureRequest.js
+++ server/routing/middleware/secureRequest.js
@@ -1,5 +1,5 @@
-module.exports = function(models) {
-  const {Session} = models;
+module.exports = function(models,withUser=false) {
+  const {User, Session} = models;
 
   return (req, res, next)=>{
     const session = req.cookies ? req.cookies.session : null;
@@ -10,7 +10,15 @@ module.exports = function(models) {
         if (err) throw err;
         if (!result) return res.status(401).send('Unauthorized')
         res.locals.session = result;
-        next();
+        if (withUser === true) {
+          User.findOne({username:result.owner},(err,user)=>{
+            if (err || !user) return res.status(500).send('Internal Error');
+            res.locals.user = user;
+            next();
+          });
+        } else {
+          next();
+        }
       });
     } else {
       res.status(401).send('Unauthorized');
