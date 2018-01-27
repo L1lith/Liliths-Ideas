1b979bf4e776a8dff9aa3d6dea68cbe7f3b2741b
L1lith <lily@lillith.pw>
2018-01-27 11:26:35 -0700

    change for to htmlfor

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -4,9 +4,9 @@ class Contact extends Component {
   render(){
     return (
       <form id='contact'>
-        <label for='subject'>Subject</label>
+        <label htmlFor='subject'>Subject</label>
         <input id='subject' name='subject' type='text'/>
-        <label for='subject'>Message</label>
+        <label htmlFor='subject'>Message</label>
         <textarea id='message' name='message'/>
         <input type='submit' value='Send'/>
       </form>
