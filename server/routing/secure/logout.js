e1ce612de906f6c127e9093027a5aed92f29294e
L1lith <lily@lillith.pw>
2018-01-26 20:47:01 -0700

    update logout endpoint to remove session cookie

--- server/routing/secure/logout.js
+++ server/routing/secure/logout.js
@@ -1,8 +1,8 @@
 function logout(app,models){
   app.get('/logout',(req,res)=>{
     res.locals.session.remove(err=>{
-      console.log('enis',err);
       if (err) return res.status(500).send('Internal Error');
+      res.clearCookie("session");
       res.status(200).send('Logged Out.');
     });
   });
