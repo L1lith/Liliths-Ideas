1575916ab614280b956e1acf71863af56d20ea81
L1lith <lily@lillith.pw>
2018-01-27 23:55:57 -0700

    update frontpage to reflect state

--- client/pages/frontpage.js
+++ client/pages/frontpage.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
 import './frontpage.less';
+import {connect} from 'react-redux';
 
 class FrontPage extends Component {
   render(){
-    return (
-      <div id='frontpage' className='frontpage'>
-
-      </div>
-    );
+    return this.props.frontPage;
   }
 }
 
-export default [FrontPage,'/',{exact:true}];
+export default [connect(state=>{frontPage:state.frontPage})(FrontPage),'/',{exact:true}];
