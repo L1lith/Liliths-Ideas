340d6fd30209e4f084b254f9b96790d1e7cd88da
L1lith <lily@lillith.pw>
2018-01-27 18:23:26 -0700

    revert to spdy

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const port = require('./config').port || 8000;
 
 if (require.main === module) {
   await (startServer());
-  console.log(`Server Listening on Port ${port}`);
 }
 
 function startServer(){
+  console.log(`Server Listening on Port ${port}`);
   return new Promise((resolve,reject)=>{
     createApp().then(app => {
-      const server = spdy.createServer({},app);
+      const server = spdy.createServer({ssl: false},app);
       server.listen(port, err => {
         if (err) return reject(err);
         resolve(server);
