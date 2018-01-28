199532ffa312ee9501043490efb344cc02af9073
L1lith <lily@lillith.pw>
2018-01-27 20:54:08 -0700

    frontpage fetch lean

--- server/routing/public/frontPage.js
+++ server/routing/public/frontPage.js
@@ -8,7 +8,7 @@ module.exports = (app,models)=>{
     if (req.query.hasOwnProperty('page') && (typeof req.query.page != 'string' || req.query.page.length < 1 || !isInteger(req.query.page))) return res.status(400).send('Malformed Request');
     const pageNumber = req.query.hasOwnProperty('page') ? parseInt(req.query.page) : 1;
     if (pageNumber < 1) return res.status(400).send('Malformed Request');
-    Post.find().skip((pageNumber - 1 ) * resultsPerPage).limit(resultsPerPage).exec((err,posts)=>{
+    Post.find().lean().skip((pageNumber - 1 ) * resultsPerPage).limit(resultsPerPage).exec((err,posts)=>{
       if (err) return res.status(500).send('Internal Error');
       res.status(200).send(posts.map(post=>{return {id:post._id,title:post.title,content:post.content,creator:post.creator,created:post.createdAt,tags:post.tags,version:post.__v}}));
     });
