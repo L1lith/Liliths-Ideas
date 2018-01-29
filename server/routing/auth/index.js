2f955de31260a7de91948430a7f38248f4f7ce80
L1lith <lily@lillith.pw>
2018-01-29 09:48:53 -0700

    add rate limiting to auth

--- server/routing/auth/index.js
+++ server/routing/auth/index.js
@@ -1,8 +1,16 @@
+const RateLimit = require('express-rate-limit');
 const express = require('express');
 const endpoints = ['login','logout','signup','validate'].map(endpoint=>require('./'+endpoint));
 
+const limiter = new RateLimit({
+  windowMs: 60*60*1000, // Hour
+  max: 25, // limit each IP to 100 requests per windowMs
+  delayMs: 0 // disable delaying - full speed until the max limit is reached
+});
+
 module.exports = function(app,models){
   const auth = express.Router();
+  auth.use(limiter);
   endpoints.forEach(endpoint=>endpoint(auth,models));
   app.use('/auth',auth);
 }
