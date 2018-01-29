307c9df4aa9d5963eb7f3e89b771c0b0f78e6cab
L1lith <lily@lillith.pw>
2018-01-29 10:46:49 -0700

    remove rate limiting from validate

--- server/routing/auth/index.js
+++ server/routing/auth/index.js
@@ -1,15 +1,17 @@
 const RateLimit = require('express-rate-limit');
 const express = require('express');
-const endpoints = ['login','logout','signup','validate'].map(endpoint=>require('./'+endpoint));
+const endpoints = ['login','logout','signup'].map(endpoint=>require('./'+endpoint));
+const validate = require('./validate');
 
 const limiter = new RateLimit({
-  windowMs: 60*60*1000, // Hour
-  max: 25, // limit each IP to 100 requests per windowMs
+  windowMs: 24*60*60*1000, // Day
+  max: 100, // limit each IP to 100 requests per windowMs
   delayMs: 0 // disable delaying - full speed until the max limit is reached
 });
 
 module.exports = function(app,models){
   const auth = express.Router();
+  validate(auth,models);
   auth.use(limiter);
   endpoints.forEach(endpoint=>endpoint(auth,models));
   app.use('/auth',auth);
