72c1b53daa99ee2e74506b02be2b6d69ac8844ff
L1lith <lily@lillith.pw>
2018-01-29 09:23:19 -0700

    update postviewer

--- client/components/postViewer.js
+++ client/components/postViewer.js
@@ -6,16 +6,43 @@ import './postViewer.less';
 const DOMPurify = createDOMPurify(window);
 
 class PostViewer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+    ['last','next'].forEach(property=>{this[property] = this[property].bind(this);});
+  }
   render(){
     if (!this.props || !this.props.posts || typeof this.props.id != 'string' || this.props.id.length < 1) return null;
     const post = this.props.posts[this.props.id];
     if (!post) return null;
+    const last = typeof this.props.onLast == 'function' ? <button className='last' disabled={this.state.disabled==='last'} onClick={this.last}>⇦</button> : null;
+    const next = typeof this.props.onNext == 'function' ? <button className='next' disabled={this.state.disabled==='next'} onClick={this.next}>⇨</button> : null;
     return (
       <div className='postviewer' id='postviewer'>
         <h1 className='title'>{post.title}</h1>
         <div className='content' dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(post.content)}}/>
+        {last}
+        {next}
       </div>
     );
+  }
+  next(){
+    if (typeof this.props.onNext != 'function') return;
+    const output = this.props.onNext();
+    if (output === false) {
+      this.setState(Object.assign({},this.state,{disabled:'next'}));
+    } else {
+      this.setState(Object.assign({},this.state,{disabled:'null'}));
+    }
+  }
+  last(){
+    if (typeof this.props.onNext != 'function') return;
+    const output = this.props.onLast();
+    if (output === false) {
+      this.setState(Object.assign({},this.state,{disabled:'last'}));
+    } else {
+      this.setState(Object.assign({},this.state,{disabled:'null'}));
+    }
   }
 }
 
--- client/pages/frontpage.js
+++ client/pages/frontpage.js
@@ -9,6 +9,7 @@ class FrontPage extends Component {
   constructor(props){
     super(props);
     this.state = {loading:true,pageNumber:1,postNumber:1};
+    ['last','next'].forEach(property=>{this[property] = this[property].bind(this);});
   }
   componentWillMount(){
     this.props.dispatch(getFrontPage(undefined,error=>{
@@ -21,11 +22,28 @@ class FrontPage extends Component {
         <Alert error={this.state.error}/>
         {this.props.frontPages[this.state.pageNumber - 1] ?
           (
-            <PostViewer id={this.props.frontPages[this.state.pageNumber - 1][this.state.postNumber - 1]}/>
+            <PostViewer onLast={this.last} onNext={this.next} id={this.props.frontPages[this.state.pageNumber - 1][this.state.postNumber - 1]}/>
           )
         : null}
       </div>
     );
+  }
+  last(){
+    console.log('last');
+    const posts = this.props.frontPages[this.state.pageNumber - 1];
+    const newPostNumber = this.state.postNumber - 1;
+    console.log(newPostNumber);
+    if (newPostNumber > 0) {
+      this.setState(Object.assign({},this.state,{postNumber:newPostNumber}));
+    }
+  }
+  next(){
+    console.log('next');
+    const posts = this.props.frontPages[this.state.pageNumber - 1];
+    const newPostNumber = this.state.postNumber + 1;
+    if (newPostNumber <= posts.length) {
+      this.setState(Object.assign({},this.state,{postNumber:newPostNumber}));
+    }
   }
 }
 
