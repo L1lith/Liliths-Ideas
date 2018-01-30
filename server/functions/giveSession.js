8827a4e664d78942f10b5c651cacee796926e4d1
L1lith <lily@lillith.pw>
2018-01-30 00:33:24 -0700

    make username a cookie

--- server/functions/giveSession.js
+++ server/functions/giveSession.js
@@ -10,6 +10,12 @@ function giveSession(res, user, models) {
         secure: process.env.NODE_ENV === 'production',
         sameSite: true
       });
+      res.cookie('username',session.owner,{
+        maxAge: 1000 * 60 * 60 * 24 * 30 * 10,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: true
+      });
       resolve(session);
     }).catch(err => reject(err));
   });
